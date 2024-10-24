8b65c587559d472e623f7da08a0e4e7ca234a17d
Arxtage <tsaturyanarmann@gmail.com>
2024-10-24 20:42:04 +0100

    Docker Compose launching both services

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,8 +26,8 @@ load_dotenv(dotenv_path='.env')
 
 app = FastAPI()
 
-# Serve the React static files
-app.mount("/static", StaticFiles(directory="/frontend/build/static", html=True), name="static")
+# Serve the React static files from the shared volume
+app.mount("/static", StaticFiles(directory="/frontend/build/static"), name="static")
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 JWT_SECRET = os.environ.get('JWT_SECRET') or secrets.token_hex(32)
